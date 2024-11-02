bc7b69d83eff162af9b19b67a1cee42ce0a3a3e3
tyson-lin <tysonlin@umich.edu>
2024-11-02 00:58:39 -0400

    x

--- wSender-base.cpp
+++ wSender-base.cpp
@@ -130,7 +130,6 @@ void sender(string r_ip, int r_port, int window_size, string input, string log_f
                     highest_ack = header.seqNum;
                 }
             }
-            cout << now << endl;
         } 
         cout << "Here 2" << endl;
         if (highest_ack > seq_num){
