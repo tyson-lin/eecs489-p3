adad3e670f5d886cd0ce7627394c422e7369d3a4
tyson-lin <tysonlin@umich.edu>
2024-11-04 00:21:01 -0500

    x

--- wSender-base.cpp
+++ wSender-base.cpp
@@ -100,7 +100,9 @@ void sender(string r_ip, int r_port, int window_size, string input, string log_f
                     highest_ack = header.seqNum;
                 }
             }
-            cout << "Timer: " << start << " " << now << endl;
+            auto start_printable = chrono::duration_cast<chrono::milliseconds>(start);
+            auto now_printable = chrono::duration_cast<chrono::milliseconds>(now);
+            cout << "Timer: " << start_printable << " " << now_printable << endl;
         } 
         cout << "Here 4" << endl;
         if (highest_ack > seq_num){
