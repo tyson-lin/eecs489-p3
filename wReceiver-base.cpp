27fc041fd59b29df05a5f15264541678cba7080d
tyson-lin <tysonlin@umich.edu>
2024-11-04 23:24:48 -0500

    x

--- wReceiver-base.cpp
+++ wReceiver-base.cpp
@@ -76,6 +76,7 @@ void receiver(int port_num, int window_size, string output_dir, string log_filen
 
                 // Setup output file
                 outfile_name = output_dir + "/File-" + to_string(connection_count) + ".out";
+                outfile_name.erase(0,1);
                 cout << "Output file location: " << outfile_name << endl;
                 outfile.open(outfile_name);
                 outfile.close();
