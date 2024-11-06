96e32619883a4aa10e8c4fd6c16d02a4080baf33
tyson-lin <tysonlin@umich.edu>
2024-11-06 00:34:14 -0500

    x

--- wReceiver-opt.cpp
+++ wReceiver-opt.cpp
@@ -1,8 +1,193 @@
 #include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "crc32.h"
+#include "PacketHeader.h"
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    cout << "Optimized Receiver" << endl;
-    return 0;
+unsigned int expected_seq_num = 0;
+bool currently_recieving = false;
+
+ofstream logfile;
+
+string outfile_name;
+ofstream outfile;
+
+struct Packet {
+    PacketHeader header;
+    char data[PACKET_SIZE];
+};
+vector<Packet> outstanding_packets;
+
+struct sockaddr_in server_addr, client_addr;
+socklen_t client_addr_len = sizeof(client_addr);
+
+unsigned int connection_count = 0;
+
+// server
+void receiver(int port_num, int window_size, string output_dir, string log_filename) {
+
+    logfile = ofstream(log_filename);
+
+    // create socket and bind to port
+    int server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;  // Bind to any available interface
+    server_addr.sin_port = htons(port_num); // Port number
+    bind(server_fd, (sockaddr*) &server_addr, sizeof(server_addr));
+
+    char buffer[PACKET_SIZE];
+
+    fd_set rfds;
+    while (1) {
+        FD_ZERO(&rfds);
+        FD_SET(server_fd, &rfds);
+        select(server_fd + 1, &rfds, NULL, NULL, NULL);
+        if (!FD_ISSET(server_fd, &rfds)){
+            continue;
+        }
+        // read packet
+        PacketHeader header;
+        bool success = recv_packet(server_fd, &client_addr, header, logfile, buffer);
+        if (!success){
+            continue;
+        }
+
+        // Receive START command and send ACK
+        if (currently_recieving == false) {
+            if (header.type == TYPE_START) {
+                // We are now receiving
+                currently_recieving = true;
+                expected_seq_num = 0;
+
+                // Setup output file
+                outfile_name = output_dir + "/FILE-" + to_string(connection_count) + ".out";
+                outfile_name.erase(0,1);
+                cout << "Output file location: " << outfile_name << endl;
+                outfile.open(outfile_name);
+                outfile.close();
+
+                // Send ACK
+                PacketHeader start_response;
+                start_response.type = TYPE_ACK;
+                start_response.seqNum = header.seqNum;
+                start_response.length = 0;
+                send_packet(server_fd, client_addr, start_response, logfile);
+            } 
+        } else if (currently_recieving == true) {
+            if (header.type == TYPE_START) {
+                continue;
+            } 
+            if (header.type == TYPE_DATA) {
+                cout << "Expected seq num: " << expected_seq_num << endl;
+                //cout << header.seqNum << " " <<  (unsigned int)expected_seq_num << " " << (unsigned int)window_size << endl;
+                // Packet is the next desired packet
+                if ((header.seqNum) == (unsigned int)expected_seq_num) {
+                    // TODO: PRINT BUFFER TO FILE
+                    expected_seq_num++;
+                    cout << "Printing packet " << header.seqNum << endl;
+                    outfile.open(outfile_name, ios::app);
+                    if (outfile.is_open()) {
+                        outfile << buffer;
+                    }
+                    outfile.close();
+                    // ======================================================================
+                    // sort through outstanding packets to check if we have anything of note
+                    while (true) {
+                        bool packet_found = false;
+                        for (size_t i = 0; i < outstanding_packets.size(); i++) {
+                            if (outstanding_packets[i].header.seqNum == expected_seq_num) {
+                                //cout << "removing packet " << outstanding_packets[i].header.seqNum << endl;
+                                expected_seq_num++;
+                                packet_found = true;
+                                cout << "Printing packet " << outstanding_packets[i].header.seqNum << endl;
+                                outfile.open(outfile_name,ios::app);
+                                if (outfile.is_open()) {
+                                    outfile << outstanding_packets[i].data;
+                                } 
+                                outfile.close();
+                                outstanding_packets.erase(outstanding_packets.begin()+i);
+                                // ======================================================================
+                                break;
+                            }
+                        }
+
+                        // no interesting packets in outstanding set
+                        if (packet_found == false) {
+                            break;
+                        }
+                    }
+                    PacketHeader ack_header = {TYPE_ACK, (unsigned int)expected_seq_num, 0, 0};
+                    send_packet(server_fd, client_addr, ack_header, logfile);
+                } else 
+                // Packet within range
+                if (header.seqNum < (unsigned int)expected_seq_num + (unsigned int)window_size) {
+                    Packet packet;
+                    packet.header = header;
+                    for (unsigned int i = 0; i < header.length; i++) {
+                        packet.data[i] = buffer[i];
+                    }
+
+                    // Add packet to list of outstanding packets ONLY if it
+                    // it's not already there
+                    bool packet_found = false;
+                    for (size_t i = 0; i < outstanding_packets.size(); i++) {
+                        if (outstanding_packets[i].header.seqNum == header.seqNum) {
+                            //cout << "already outstanding" << endl;
+                            packet_found = true;
+                        }
+                    }
+                    if (packet_found == false) {
+                        //cout << "adding packet " << packet.header.seqNum << endl;
+                        outstanding_packets.push_back(packet);
+                    } 
+                    PacketHeader ack_header = {TYPE_ACK, (unsigned int)expected_seq_num, 0, 0};
+                    send_packet(server_fd, client_addr, ack_header, logfile);
+                }
+            } 
+            if (header.type == TYPE_END) {
+
+                // generate end response
+                PacketHeader end_response;
+                end_response.type = TYPE_ACK;
+                end_response.seqNum = header.seqNum;
+                end_response.length = 0;
+                send_packet(server_fd, client_addr, end_response, logfile);
+
+                // set internal state
+                currently_recieving = false;
+
+                // increase connection count
+                connection_count++;
+            } 
+            if (header.type == TYPE_ACK) {
+                // shouldn't happen
+                continue;
+            } 
+        }
+         
+        cout << "==================================" << endl << endl;
+    }
+}
+
+// ./wReceiver <port-num> <window-size> <output-dir> <log>
+int main (int argc, char* argv[]) {
+    if (argc != 5) {
+        cout << "Usage: ./wReceiver <port-num> <window-size> <output-dir> <log>\n";
+        return 1;
+    }
+    receiver(atoi(argv[1]), atoi(argv[2]), argv[3], argv[4]);
 }
--- wSender-opt.cpp
+++ wSender-opt.cpp
@@ -1,8 +1,141 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+#include <cmath>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <fcntl.h>
+#include <cassert>
+#include <chrono>
+#include <unistd.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include "PacketHeader.h"
+#include "crc32.h"
+#include <sys/time.h>
+#include <ctime>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    cout << "Optimized Sender" << endl;
-    return 0;
+struct sockaddr_in server_addr, client_addr;
+
+unsigned int start_seq_num = 0;
+
+ofstream logfile;
+
+int send_data_packet(int client_fd, int seqNum, string& s, int index){
+    string data;
+    PacketHeader header;
+    header.type = 2;
+    header.seqNum = seqNum;
+    header.length = min(DATA_SIZE, (int)s.size() - index);
+    cout << "Size: " << (int)s.size() - index << endl;
+    data = s.substr(index, header.length);
+    index += header.length;
+    send_packet(client_fd, server_addr, header, logfile, data.c_str());
+    return index;
+}
+
+void sender(string r_ip, int r_port, unsigned int window_size, string input, string log_filename){
+    logfile = ofstream(log_filename);
+
+    int fd = open(input.c_str(), 0);
+
+    string s = "";
+    char buf;
+    while (true){
+        int bytes = read(fd, &buf, 1);
+        if (bytes < 1 || buf == '\0'){
+            break;
+        }
+        s += buf;
+    }
+    
+    //cout << "buffer = \n" << s << '\n';
+    unsigned int num_packets = (int)ceil((double) s.size() / (DATA_SIZE));
+    cout << s.size() << " bytes to send, " << num_packets << " packets" << endl;
+    vector<int> start_indices(num_packets, 0); // Initialize vector of size n with all elements as -1
+    for (unsigned int i = 1; i < num_packets; i++) {
+        start_indices[i] = start_indices[i-1] + DATA_SIZE;
+    }
+
+    int client_fd  = socket(AF_INET, SOCK_DGRAM, 0);
+    const int enable = 1;
+    setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+
+    // Make socket address
+    server_addr.sin_family = AF_INET;              // IPv4
+    server_addr.sin_addr.s_addr = inet_addr(r_ip.c_str());  // Server IP address
+    server_addr.sin_port = htons(r_port);            // Port number (convert to network byte order)
+    //bind(server_fd, (sockaddr*) &server_addr, sizeof(server_addr));
+
+    // Connect to server
+    cout << "connected to " << r_ip << endl;
+    PacketHeader header;
+    header.type = 0;
+    header.length = 0;
+    srand((int)time(0));
+    start_seq_num = rand();
+    header.seqNum = start_seq_num;
+    send_packet(client_fd, server_addr, header, logfile);
+
+    cout << "start sent" << endl;
+
+    char data[DATA_SIZE];
+    recv_packet(client_fd, &server_addr, header, logfile, data);
+
+    cout << "ack recved" << endl;
+    
+    unsigned int highest_ack = 0;
+    unsigned int seq_num = 0;
+    fd_set rfds;
+
+    while (highest_ack != num_packets) {
+        unsigned int w_size = min(window_size, num_packets - seq_num);
+        for (unsigned int i = seq_num; i < seq_num + w_size; ++i){
+            send_data_packet(client_fd, i, s, start_indices[i]);
+        }
+        auto start = std::chrono::steady_clock::now();
+        auto now = start;
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
+        while (duration.count() < 500 && highest_ack <= seq_num){
+            FD_ZERO(&rfds);
+            FD_SET(client_fd, &rfds);
+            timeval timeout;
+            timeout.tv_sec = 0;
+            timeout.tv_usec = 500000;
+            select(client_fd + 1, &rfds, NULL, NULL, &timeout);
+            if (FD_ISSET(client_fd, &rfds)){
+                recv_packet(client_fd, &server_addr, header, logfile, data);
+                if (header.type == 3){
+                    highest_ack = header.seqNum;
+                    start = std::chrono::steady_clock::now();
+                }
+            }
+            now = std::chrono::steady_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
+        } 
+        
+        if (highest_ack > seq_num){
+            seq_num = highest_ack;
+        } 
+    }
+    header.type = 1;
+    header.seqNum = start_seq_num;
+    header.length = 0;
+    send_packet(client_fd, server_addr, header, logfile);
+    recv_packet(client_fd, &server_addr, header, logfile, data);
+    close(client_fd);
+}
+
+int main (int argc, char* argv[]){
+    sender(argv[1], atoi(argv[2]), atoi(argv[3]), argv[4], argv[5]);
 }
